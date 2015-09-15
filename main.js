46f30ec72f07c94456ae8eb3a65beebb5df79829
Joe Ziemba <jziemba90@gmail.com>
2015-09-14 23:18:55 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,14 @@
+
+
+for (i = 0; i < 12; i++) {
+	result = i * 9
+
+	console.log("i * 9 = " + result)
+	
+};
+
+students = ['Lucille', 'Michael', 'George Michael', 'Joe', 'Chris', 'Warren']
+
+for (i = 0; i < students.length; i++) {
+	console.log(students[i] + " is in the class.")
+};
